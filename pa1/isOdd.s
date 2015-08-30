40932773b5055b03da53f4d179847ae60c941b6e
Ivan Wick <ivanwick+github@gmail.com>
2015-08-30 16:58:49 -0700

    Implemented isOdd

--- pa1/isOdd.s
+++ pa1/isOdd.s
@@ -3,4 +3,17 @@
 	.global _isOdd
 
 _isOdd:
+	movl %edi, %eax
+	xor %edx, %edx # clear edx, top bits of the operand used by idivl
+	movl $2, %esi
+
+	# idivl is a SIGNED division as opposed to divl unsigned
+	idivl %esi
+
+	# %edx has the remainder
+	# no additional processing necessary here because
+	# 1: true, it is odd
+	# 0: false, it's not odd
+
+	movl %edx, %eax
 	retq
