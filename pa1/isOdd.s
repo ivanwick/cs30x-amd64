cae5c41bc1d2670dd3b6a556d87d3770c66056d3
Ivan Wick <ivanwick+github@gmail.com>
2015-08-30 19:12:48 -0700

    Reworked isOdd - Proper size, the C declaration says long, so use 8-byte registers - Handle negative numbers better with sign-extension before dividing

--- pa1/isOdd.s
+++ pa1/isOdd.s
@@ -3,17 +3,24 @@
 	.global _isOdd
 
 _isOdd:
-	movl %edi, %eax
-	xor %edx, %edx # clear edx, top bits of the operand used by idivl
-	movl $2, %esi
+	movq	%rdi, %rax
+	# sign-extend %rax into %rdx:%rax, idivl uses it like that
+	cqto
+	movq	$2, %rsi
 
-	# idivl is a SIGNED division as opposed to divl unsigned
-	idivl %esi
+	# idivl is a SIGNED division as opposed to the unsigned divq
+	idivq	%rsi
 
 	# %edx has the remainder
-	# no additional processing necessary here because
 	# 1: true, it is odd
 	# 0: false, it's not odd
+	# But we can't just return it unmodified because it might be negative.
+	# So we need some logic here to regularize the return value to (0, 1)
+	cmpq	$0, %rdx
+	jz	end
+	movq	$1, %rdx
 
+end:
+	# returns int so movl is ok here instead of movq
 	movl %edx, %eax
 	retq
