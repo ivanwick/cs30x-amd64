cfa4d61c66a37c63b8e06468f5f64b3ea4b928a6
Ivan Wick <ivanwick+github@gmail.com>
2015-08-30 22:02:56 -0700

    Implemented numOfDigits

--- pa1/numOfDigits.s
+++ pa1/numOfDigits.s
@@ -21,4 +21,68 @@
 	.global _numOfDigits
 
 _numOfDigits:
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+	# This is not a leaf function so we'll need to store callee-saved regs
+	# Allocate 16 bytes for the two longs we need to save.
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp) # long num
+	movl	%esi, -16(%rbp) # int base
+
+	# We'll be calling a function below but don't worry about caller-saved
+	# regs because we haven't done anything yet that needs to be saved.
+
+	# Function call to do a range check on the int base argument
+	movslq	%esi, %rdx # long value (do now because we overwrite %rsi below)
+	movq	$2, %rdi # long minRange
+	movq	$36, %rsi # long maxRange
+	callq	_checkRange
+
+	movl	%eax, %ecx # move the return value aside
+	movl	$-1, %eax # threaten to return -1 based on the comparison
+	cmpl	$0, %ecx
+	jz	end
+
+	# get our saved arguments back into the registers
+	movq	-8(%rbp), %rdi # long num
+	movl	-16(%rbp), %esi # int base
+
+	# special case, numOfDigits(0, x): 1
+	# %ecx is the counter in the loop below which we'll be returning
+	movl	$1, %eax
+	cmpq	$0, %rdi # passed in as 8-byte long
+	jz	end
+
+loopinit:
+	# Intiailize %rax to the num argument so that we can repeatedly idivq
+	# and it will put the result back into %rax.
+	movq	%rdi, %rax
+	# Initialize %ecx as a counter of how many times we divided.
+	movl	$0, %ecx
+
+	# %esi came in as a 4-byte int but we need to use it later to idivq.
+	# Be explicit about promoting it with sign extension (even though we
+	# have the range check above).
+	movslq	%esi, %rsi
+
+loopcontinue:
+	cmpq	$0, %rax
+	jz	loopfinish
+
+	# Sign-extend %rax into oct word %rdx:%rax because idivq uses that.
+	# A previous wrong version zeroed-out %rdx here but it did not work
+	# when %rax was negative.
+	cqto
+
+	idivq	%rsi
+	incl	%ecx # we divided again so increment the counter
+
+	jmp	loopcontinue
+
+loopfinish:
+	movl	%ecx, %eax
+end:
+	addq	$16, %rsp
+	popq	%rbp
 	retq
