37744a290cd41834ec5470638c0dd7797164c561
Ivan Wick <ivanwick+github@gmail.com>
2015-09-01 18:40:44 -0700

    Implement printCharRepeat to abstract some looping

--- pa1/displayDiamond.s
+++ pa1/displayDiamond.s
@@ -4,3 +4,31 @@
 
 _displayDiamond:
 	retq
+
+
+# void printCharRepeat( char c, long times )
+_printCharRepeat:
+	pushq	%rbp
+	movq	%rsp, %rbp
+
+	# Save our parameters
+	# Not packed but we need to reserve 16 bytes of stack space to
+	# maintain alignment anyway
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+
+Lreploop:
+	cmpq	$0, -16(%rbp)
+	jbe	Lrepend
+
+	movl	-4(%rbp), %edi
+	callq	_printChar
+
+	subq	$1, -16(%rbp)
+	jmp	Lreploop
+
+Lrepend:
+	addq	$16, %rsp
+	popq	%rbp
+	retq
